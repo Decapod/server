9ded84a09feac623614c348e2fc84fa48ebb7a12
obara justin <obara.justin@gmail.com>
2010-06-07 11:27:14 -0400

    DECA-109: Updating the preliminary tests.
    Added some documentation, removed an unnecessary file by moving the setup_mockserver function into test_server as setup_server, and some minor updates to the tests including adding a test to make sure the redirect url for the first test is correct.

--- tests/test_server.py
+++ tests/test_server.py
@@ -6,47 +6,81 @@ import simplejson
 import sys
 sys.path.append(os.path.abspath('..'))
 import mockserver
-import servertest as stest
+from cherrypy.test import helper
 
 current_dir = os.path.abspath(os.path.dirname(__file__))
 
+# must be called setup_server
 def setup_server():
-    stest.setup_mockserver()
+    """ Starts the server, using the cherrypy.tree.mount function """
+    
+    parent = os.path.abspath(os.path.dirname('../'))
+    configPath = os.path.join(parent, 'dserver.conf')
+    root = mockserver.MockServer()
+    root.images = mockserver.ImageController()
+    root.pdf = mockserver.Export()
+    cherrypy.tree.mount(root, "/", configPath)
 
-class TestRequests(stest.ServerTest):
+class TestRequests(helper.CPWebCase):
+    
+    # 
     jsonHeader = [("Accept", "application/json")]
     
-    def assertJSON(self, length=None):
+    def assertJSON(self, numKeys=None):
+        """ 
+        A convenience function for testing the json data returned from a request. 
+        Currently only tests that the Content-Type is 'application/json' and the 
+        length matches the numKeys arguement 
+        """
+        
         self.assertHeader("Content-Type", "application/json")
         json = simplejson.loads(self.body)
-        if not length == None:
-            self.assertEqual(len(json), length)
+        if not numKeys == None:
+            self.assertEqual(len(json), numKeys)
         return json
     
     def test_00_getCapture(self):
+        """ 
+        Ensures that the /capture page is mounted correctly and that / redirects
+        to /capture
+        """
+        
+        captureMount = "/capture"
+        
         #tests the request for the capture page
         self.getPage("/")
         self.assertStatus(303)
+        self.assertHeader('Location', cherrypy.url(captureMount))
         
         #throws a 500 error because the capture.html page isn't available.
         #will need the ui code here as well for this to work
-        result = self.getPage("/capture")
+        self.getPage(captureMount)
         self.assertStatus(200)
 
         
     def test_01_getCameras(self):
+        """
+        Tests the GET request for retrieving the camera info
+        """
         #tests the GET request for retrieving the camera info
         self.getPage("/cameras")
         self.assertStatus(200)
         self.assertJSON(2)
         
-    def test_02_getImages_empty(self):
+    def test_02_getInitialImages_empty(self):
+        """
+        Tests the GET request for retrieving the images.
+        Since this tests the initial state, the result should indicate that
+        no images were present.
+        """
         self.getPage("/images/")
         self.assertStatus(200)
         self.assertJSON(0)
     
     def test_03_postImages(self):
-        #tests the POST request for adding images
+        """
+        Tests the POST request for adding images.
+        """
         postBody = ""
         self.getPage("/images/", method="POST", body=postBody, headers=self.jsonHeader)
         self.assertStatus(200)
@@ -57,7 +91,9 @@ class TestRequests(stest.ServerTest):
         self.failUnless(json["right"])
     
     def test_04_postPDF(self):
-        #tests the POST request for generating a PDF
+        """
+        Tests the POST request for generating a PDF
+        """
         postBody = ""
         self.getPage("/pdf", method="POST", body=postBody, headers=[("Accept", "*/*")])
         self.assertStatus(200)
