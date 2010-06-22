cba221b6fe1560359fa95eed8926718ba86356ba
colinbdclark <colinbdclark@localhost>
2010-06-22 19:49:05 -0400

    NOJIRA: Adding ResourceSource tests to test.py so everything gets run as a suite.

--- tests/test.py
+++ tests/test.py
@@ -6,7 +6,7 @@ from cherrypy.test import test as cptest
 
 def run():
     #exectues tests suites
-    test_list = ['test_server']
+    test_list = ["test_server", "test_resourcesource"]
     cptest.CommandLineParser(test_list).run()
 
 if __name__ == '__main__':
