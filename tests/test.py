e6bd3c1f53bcc5eacf28736e66d0198b6c81cac1
Justin Obara <obara.justin@gmail.com>
2012-01-23 14:32:54 -0800

    DECA-221: Refactored test.py to use Nose to find and execute all of the test scripts. By having this run in a python script, the changes that had been made to testutils.py for the url rewriting are no longer needed and have been removed.

--- tests/test.py
+++ tests/test.py
@@ -1,23 +1,8 @@
-import os.path
-import sys
-import shutil
-from cherrypy.test import test as cptest
-import testutils
+import nose
 
-def run():
-    # Update this list with new tests cases.
-    # TODO: Do this dynamically by trawling the filesystem looking for test_*.py files.
-    test_list = [
-        "test_decapod_utilities",
-        "test_resourcesource",
-        "test_imageImport"
-    ]
-    
-    cptest.TestHarness(test_list).run()
-    testutils.deleteTestImagesDir()
-    testutils.cleanUpDir("../book/images/")
-    shutil.rmtree("../book/pdf/")
-    
+# Run as a normal python script
+# Nose will automatically find all test scripts
+  
 if __name__ == '__main__':
-    run()
+    nose.main()
     
--- tests/testutils.py
+++ tests/testutils.py
@@ -6,21 +6,8 @@ import mimetypes
 sys.path.append(os.path.abspath('..'))
 import resourcesource
 
-TEST_DIR_NAME = "tests"
 CONFIG_PATH = "data/resource-source-test-data.json"
 IMAGES_TEST_DIR = "data/book/images/"
-
-def getCWDName():
-    cwd = os.getcwd()
-    return os.path.split(cwd)[1]
-
-def getTestSafePath(path):
-    '''
-    Needed to fix paths between individual and package test runs.
-    When running the whole suite of tests, the working directory is one level higher.
-    '''
-    dir = getCWDName()
-    return path if dir == TEST_DIR_NAME else os.path.join(TEST_DIR_NAME, path)
 
 def cleanUpDir(dir):
     filePaths = glob.glob(dir + "/*")
@@ -28,13 +15,13 @@ def cleanUpDir(dir):
         os.remove(filePath)  
         
 def cleanUpImages():
-    cleanUpDir(getTestSafePath(IMAGES_TEST_DIR))
+    cleanUpDir(IMAGES_TEST_DIR)
 
 def deleteTestImagesDir():
-    shutil.rmtree(getTestSafePath(IMAGES_TEST_DIR))
+    shutil.rmtree(IMAGES_TEST_DIR)
     
 def createTestResourceSource():
-    return resourcesource.ResourceSource(getTestSafePath(CONFIG_PATH))
+    return resourcesource.ResourceSource(CONFIG_PATH)
 
 class mockFileStream(object):
     def __init__(self, filePath):
