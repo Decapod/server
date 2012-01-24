05f7cec2112be951806dc42569cffb80d874dbb5
Justin Obara <obara.justin@gmail.com>
2012-01-23 19:29:34 -0800

    DECA-221: Added tests for the library and book controllers. The book tests currently fail due to a bug in the book.py script. This will be addressed as part of DECA-226

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,36 +1,63 @@
 import cherrypy
 from cherrypy.test import helper
-
-import os.path
+import shutil
+import os
 import sys
 sys.path.append(os.path.abspath('..'))
 import decapod
 
-class TestREST(helper.CPWebCase):
-        
-    def setup_server():
-        decapod.mountApp()
-    setup_server = staticmethod(setup_server)
-    
     # Decapod 0.5 REST URL Spec 
     #    /: GET
     #    /library/: NONE
     #    /library/'bookName': DELETE
     #    /library/'bookName'/pages: POST
     #    /library/'bookName'/export: GET, PUT, DELETE
+
+BOOK_DIR = os.path.normpath(os.path.join(os.getcwd(), "../book"))
+
+class TestREST(helper.CPWebCase):
+    
+    def setup_server():
+        if not os.path.exists(BOOK_DIR):
+            os.mkdir(BOOK_DIR)
+        decapod.mountApp()
+    setup_server = staticmethod(setup_server)
+    
+    def teardown_server():
+        if os.path.exists(BOOK_DIR):
+            shutil.rmtree(BOOK_DIR)
+            os.mkdir(BOOK_DIR)
+    teardown_server = staticmethod(teardown_server)
+    
+    def unsupportedHTTPMethodsTest(self, url, methods):
+        '''
+        Tests that unsuppored http methods return a 405
+        '''     
+        for method in methods:
+            self.getPage(url, method=method)
+            self.assertStatus(405, "Should return a 405 'Method not Allowed' status for '{0}'".format(method))
      
-    def test_01_getRoot(self):
-#        self.setup_class()
-#        cherrypy.engine.block()
+    def test_01_root(self):
+        rootURL = "/"
         expectedRedirectURL = "/components/import/html/Import-05a.html"
-        
-        self.getPage("/")
+        self.getPage(rootURL)
         self.assertStatus(303, "Should return a 303 'See Other' status for the redirect")
         self.assertHeader("Location", cherrypy.url(expectedRedirectURL), "Assert that the Location is set to the redirect URL")
+        self.unsupportedHTTPMethodsTest(rootURL, ["PUT", "POST", "DELETE"])
         
     def test_02_redirectURL(self):
         expectedRedirectURL = "/components/import/html/Import-05a.html"
-        
         self.getPage(expectedRedirectURL)
         self.assertStatus(200, "Should return a 200 'OK' status")
+        
+    def test_03_library(self):
+        libraryURL = "/library/"
+        self.unsupportedHTTPMethodsTest(libraryURL, ["GET", "PUT", "POST", "DELETE"])
+        
+    # TODO: Tests that something existed before the deletion
+    def test_04_book(self):
+        bookURL = "/library/bookName/"
+        self.getPage(bookURL, method="DELETE")
+        self.unsupportedHTTPMethodsTest(bookURL, ["GET", "PUT", "POST"])
+        self.assertFalse(os.path.exists(BOOK_DIR), "The test directory (at path: {0}) should have been removed".format(BOOK_DIR))
         
