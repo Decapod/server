2f39bf439820250513379d2843aa33d9f02d602f
Justin Obara <obara.justin@gmail.com>
2012-01-25 11:33:51 -0800

    DECA-221: Refactored and expanded the resourcesource tests

--- tests/test_resourcesource.py
+++ tests/test_resourcesource.py
@@ -14,83 +14,106 @@ class ResourceSourceTest(unittest.TestCase):
     def setUp(self):
         self.resourceSource = testutils.createTestResourceSource()
     
-    def test_parsePathHead(self):
+    def test_01_parsePathHead(self):
         head = resourcesource.parsePathHead("${js}/foo/bar.jpg")
         self.assertEquals("${js}/foo/", head)
         
-    def test_loadConfig(self):
-        self.assertEquals(5, len(self.resourceSource.resources))
-    
-    def test_filePathForResource(self):
-        jsPath = self.resourceSource.filePathForResource("js")
-        self.assertEquals(jsPath, JS_PATH_ROOT)
-        
-        # Test an invalid resource name
-        invalidPath = self.resourceSource.filePathForResource("invalid")
-        self.assertEquals(invalidPath, None)
-        
-    def test_parseFileName(self):
+    def test_02_parseFileName(self):
         name = resourcesource.parseFileName("${js}/foo/bar.jpg")
         self.assertEquals(2, len(name))
         self.assertEquals("bar", name[0])
         self.assertEquals("jpg", name[1])
         
-    def test_filePath(self):
-        # Test just the resource token
+    def test_03_appendSuffix(self):
+        expectedPath = "${js}/foo/bar-test.jpg"
+        actualPath = resourcesource.appendSuffix("${js}/foo/bar.jpg", "-test")
+        self.assertEquals(expectedPath, actualPath)
+        
+    def test_04_loadConfig(self):
+        self.assertEquals(5, len(self.resourceSource.resources))
+    
+    def test_05_filePathForResource_valid(self):
+        jsPath = self.resourceSource.filePathForResource("js")
+        self.assertEquals(jsPath, JS_PATH_ROOT)
+        
+    def test_06_filePathForResources_invalid(self):
+        # Test an invalid resource name
+        invalidPath = self.resourceSource.filePathForResource("invalid")
+        self.assertEquals(invalidPath, None)
+        
+    def test_07_filePath_noToken(self):
+        invalidURL = self.resourceSource.filePath("invalid")
+        self.assertEquals(invalidURL, None)
+        
+    def test_08_filePath_invalidToken(self):
+        invalidURL = self.resourceSource.filePath("invalid")
+        self.assertEquals(invalidURL, None)
+        
+    def test_09_filePath_validToken(self):
         path = self.resourceSource.filePath("${js}")
         self.assertEquals(path, JS_PATH_ROOT)
         
-        # Test the resource token with additional path segments
+    def test_10_filePath_segments(self):
         path = self.resourceSource.filePath("${js}/cat/dog")
         self.assertEquals(path, JS_PATH_ROOT + "/cat/dog")
         
-        # Test with a file at the end of the path 
+    def test_11_filePath_file(self):
         path = self.resourceSource.filePath("${js}/cat/dog/hamster.js")
         self.assertEquals(path, JS_PATH_ROOT + "/cat/dog/hamster.js")
-
-        # Test with no resource token
-        invalidURL = self.resourceSource.filePath("invalid")
-        self.assertEquals(invalidURL, None)
-              
-        # Test with an invalid token name
-        invalidURL = self.resourceSource.filePath("${invalid}/cat/dog")
-        self.assertEquals(invalidURL, None)
-        
-    def test_virtualPath(self):
+    
+    def test_12_virtualPath_valid(self):
         absolutePath = JS_PATH_ROOT + "/cat/dog"
         result = self.resourceSource.virtualPath(absolutePath)
         self.assertEquals(result, "${js}/cat/dog")
-        
+                          
+    def test_13_virtualPath_invalid(self):
         absolutePath = "/invalid/path/cat/dog"
         result = self.resourceSource.virtualPath(absolutePath)
         self.assertEquals(result, absolutePath)
+    
+    def test_14_parseVirtualPath_none(self):
+        absolutePath = None
+        resourceName, pathSegs = self.resourceSource.parseVirtualPath(absolutePath)
+        self.assertEquals(None, resourceName)
+        self.assertEquals(None, pathSegs)
         
-    def test_webURLForResource(self):
+    def test_15_parseVirtualPath_invalid(self):
+        absolutePath = "invalid/cat/dog"
+        resourceName, pathSegs = self.resourceSource.parseVirtualPath(absolutePath)
+        self.assertEquals(None, resourceName)
+        self.assertEquals(None, pathSegs)
+        
+    def test_16_parseVirtualPath_valid(self):
+        absolutePath = "${js}/cat/dog"
+        resourceName, pathSegs = self.resourceSource.parseVirtualPath(absolutePath)
+        self.assertEquals("js", resourceName)
+        self.assertEquals(["cat", "dog"], pathSegs)
+        
+    def test_17_webURLForResource_valid(self):
         jsURL = self.resourceSource.webURLForResource("js")
         self.assertEquals(jsURL, "/js")
         
-        # Test an invalid resource name
+    def test_18_webURLForResource_invalid(self):
         invalidURL = self.resourceSource.webURLForResource("invalid")
         self.assertEquals(invalidURL, None)
+    
+    def test_19_weburl_noToken(self):
+        invalidURL = self.resourceSource.webURL("invalid")
+        self.assertEquals(invalidURL, None)
         
-    def test_webURL(self):
-        # Test just the resource token
+    def test_20_weburl_invalidToken(self):
+        invalidURL = self.resourceSource.webURL("${invalid}/cat/dog")
+        self.assertEquals(invalidURL, None)
+    
+    def test_21_weburl_token(self):
         url = self.resourceSource.webURL("${js}")
         self.assertEquals(url, "/js")
         
-        # Test the resource token with additional path segments
+    def test_22_weburl_segments(self):
         url = self.resourceSource.webURL("${js}/cat/dog")
         self.assertEquals(url, "/js/cat/dog")
-        
-        # Test with no resource token
-        invalidURL = self.resourceSource.webURL("invalid")
-        self.assertEquals(invalidURL, None)
-              
-        # Test with an invalid token name
-        invalidURL = self.resourceSource.webURL("${invalid}/cat/dog")
-        self.assertEquals(invalidURL, None)
 
-    def test_cherryPyConfig(self):
+    def test_23_cherryPyConfig(self):
         config = self.resourceSource.cherryPyConfig()
         self.assertEquals(6, len(config))
         
