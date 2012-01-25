38524b88821559e5ca1612dddd52b399a324ecfc
Justin Obara <obara.justin@gmail.com>
2012-01-25 09:15:40 -0800

    DECA-221: Refactored and expanded decapod_utilites tests

--- tests/test_decapod_utilities.py
+++ tests/test_decapod_utilities.py
@@ -6,44 +6,9 @@ import glob
 sys.path.append(os.path.abspath('..'))
 import decapod_utilities as utils
 
-class ResourceSourceTest(unittest.TestCase):
-    
-    testDir = "test_decapod_utilities_temp_dir"
-
-    def tearDown(self):
-        if os.path.exists(self.testDir):
-            shutil.rmtree(self.testDir)
-            
-    def checkForDir(self, path, originalNumFiles):           
-        self.assertTrue(os.path.exists(self.testDir),
-                        "The test directory should be there.")
-        self.assertTrue(os.path.isdir(self.testDir),
-                        "The path created should actually be a directory.")
-        numFiles = len(glob.glob("*"))
-        self.assertEquals(numFiles, originalNumFiles + 1,
-                          "There should be only one new change in the current directory.")
-            
-    def test_mkdirIfNecessary(self):
-        # Sanity check first.
-        self.assertFalse(os.path.exists(self.testDir),
-                         "test_mkdirIfNecessary needs a clean environment, \
-                         but the test dir already exists")
-        originalNumFiles = len(glob.glob("*"))
-
-         # Try making a new directory.
-        utils.mkdirIfNecessary(self.testDir)
-        self.checkForDir(self.testDir, originalNumFiles)
-
-        # Try again. Nothing special should happen.
-        utils.mkdirIfNecessary(self.testDir)
-        self.checkForDir(self.testDir, originalNumFiles)
-
-        # Now delete and try again.
-        shutil.rmtree(self.testDir)
-        utils.mkdirIfNecessary(self.testDir)
-        self.checkForDir(self.testDir, originalNumFiles)
+class CommandInvokationTests(unittest.TestCase):
                         
-    def test_invokeCommandSync(self):
+    def test_01_invokeCommandSync_valid(self):
         # Test a basic command line program
         expectedOutput = "Hello Test!"
         cmd = ["echo", expectedOutput]
@@ -52,13 +17,40 @@ class ResourceSourceTest(unittest.TestCase):
                                          "An error occurred while invoking a command line program")
         self.assertEquals(expectedOutput + "\n", output)
         
-        # Now test a program that doesn't exist.
+    def test_02_invokeCommandSync_invalid(self):
+        # Test a program that doesn't exist.
         cmd = ["this_command_doesnt_exist", "--foo"]
-        try:
-            output = utils.invokeCommandSync(cmd,
-                                             Exception,
-                                             "invokeCommand correctly throws an exception")
-            self.fail("Invoking an invalid command should not succeed.")
-        except Exception:
-            self.assertTrue(True,
-                            "An exception should be thrown when trying to invoke an invalid command.")
+        self.assertRaises(Exception, utils.invokeCommandSync, cmd, Exception, "inovkeCommand correctly throws an exception")
+
+class DirectoryCreationTests(unittest.TestCase):
+    
+    newTestDir = os.path.abspath("new_dir")
+    existingTestDir = os.path.abspath("existing_dir")
+    
+    def setUp(self):
+        if not os.path.exists(self.existingTestDir):
+            os.mkdir(self.existingTestDir)
+    
+    def tearDown(self):
+        if os.path.exists(self.newTestDir):
+            shutil.rmtree(self.newTestDir)
+            
+    def assertDirExists(self, path):           
+        self.assertTrue(os.path.exists(path), "The test directory should be there.")
+        self.assertTrue(os.path.isdir(path), "The path created should actually be a directory.")
+            
+    def test_01_mkdirIfNecessary_create(self):
+        utils.mkdirIfNecessary(self.newTestDir)
+        self.assertDirExists(self.newTestDir)
+        
+    def test_02_mkdirIfNecessary_existing(self):
+        utils.mkdirIfNecessary(self.existingTestDir)
+        self.assertDirExists(self.existingTestDir)
+            
+    def test_03_remakeDir_create(self):
+        utils.remakeDir(self.newTestDir)
+        self.assertDirExists(self.newTestDir)
+
+    def test_04_remakeDir_existing(self):
+        utils.remakeDir(self.existingTestDir)
+        self.assertDirExists(self.existingTestDir)
