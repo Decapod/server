05a5b88ee697c475ad2eb3d89e0a3acdb909807c
Justin Obara <obara.justin@gmail.com>
2012-01-25 04:46:52 -0800

    DECA-221: Refactored the cherrypy server tests to be in different test classes. This will make it less prone to interactions between tests and allow for more customization of setups.

--- tests/test_server.py
+++ tests/test_server.py
@@ -15,51 +15,69 @@ import decapod
 
 BOOK_DIR = os.path.normpath(os.path.join(os.getcwd(), "../library/book"))
 
-class TestREST(helper.CPWebCase):
+def setup_server():
+    if not os.path.exists(BOOK_DIR):
+        os.mkdir(BOOK_DIR)
+    decapod.mountApp()
     
-    def setup_server():
-        if not os.path.exists(BOOK_DIR):
-            os.mkdir(BOOK_DIR)
-        decapod.mountApp()
-    setup_server = staticmethod(setup_server)
-    
-    def teardown_server():
-        if os.path.exists(BOOK_DIR):
-            shutil.rmtree(BOOK_DIR)
-            os.mkdir(BOOK_DIR)
-    teardown_server = staticmethod(teardown_server)
-    
-    def unsupportedHTTPMethodsTest(self, url, methods):
+def teardown_server():
+    if os.path.exists(BOOK_DIR):
+        shutil.rmtree(BOOK_DIR)
+        os.mkdir(BOOK_DIR)
+
+class serverTests(helper.CPWebCase):
+    '''
+    A subclass of helper.CPWebCase
+    The purpose of this class is to add new common test functions that can be easily shared
+    by various test classes.
+    '''
+    def assertUnsupportedHTTPMethods(self, url, methods):
         '''
         Tests that unsuppored http methods return a 405
         '''     
         for method in methods:
             self.getPage(url, method=method)
             self.assertStatus(405, "Should return a 405 'Method not Allowed' status for '{0}'".format(method))
-     
-    def test_01_root(self):
-        rootURL = "/"
-        expectedRedirectURL = "/components/import/html/Import-05a.html"
-        self.getPage(rootURL)
+
+class TestRoot(serverTests):
+    
+    setup_server = staticmethod(setup_server)
+    teardown_server = staticmethod(teardown_server)
+    rootURL = "/"
+    expectedRedirectURL = "/components/import/html/Import-05a.html"
+    
+    def test_01_get(self):
+        self.getPage(self.rootURL)
         self.assertStatus(303, "Should return a 303 'See Other' status for the redirect")
-        self.assertHeader("Location", cherrypy.url(expectedRedirectURL), "Assert that the Location is set to the redirect URL")
-        self.unsupportedHTTPMethodsTest(rootURL, ["PUT", "POST", "DELETE"])
+        self.assertHeader("Location", cherrypy.url(self.expectedRedirectURL), "Assert that the Location is set to the redirect URL")
         
-    def test_02_redirectURL(self):
-        expectedRedirectURL = "/components/import/html/Import-05a.html"
-        self.getPage(expectedRedirectURL)
+    def test_02_unsupportedMethods(self):
+        self.assertUnsupportedHTTPMethods(self.rootURL, ["PUT", "POST", "DELETE"])
+        
+    def test_03_redirectURL(self):
+        self.getPage(self.expectedRedirectURL)
         self.assertStatus(200, "Should return a 200 'OK' status")
+
+class TestLibrary(serverTests):
+    
+    setup_server = staticmethod(setup_server)
+    teardown_server = staticmethod(teardown_server)
         
-    def test_03_library(self):
+    def test_01_unsupportedMethods(self):
         libraryURL = "/library/"
-        self.unsupportedHTTPMethodsTest(libraryURL, ["GET", "PUT", "POST", "DELETE"])
+        self.assertUnsupportedHTTPMethods(libraryURL, ["GET", "PUT", "POST", "DELETE"])
+
+class TestBook(serverTests):
+    
+    setup_server = staticmethod(setup_server)
+    teardown_server = staticmethod(teardown_server)
         
     # TODO: Tests that something existed before the deletion
-    def test_04_book(self):
+    def test_01_book(self):
         bookURL = "/library/bookName/"
         self.assertTrue(BOOK_DIR, "The 'book' directory (at path: {0}) should currently exist".format(BOOK_DIR))
         self.getPage(bookURL, method="DELETE")
         self.assertFalse(os.path.exists(BOOK_DIR), "The 'book' directory (at path: {0}) should have been removed".format(BOOK_DIR))
-        self.unsupportedHTTPMethodsTest(bookURL, ["GET", "PUT", "POST"])
+        self.assertUnsupportedHTTPMethods(bookURL, ["GET", "PUT", "POST"])
         
         
