1082fed748f4f97bdc479315c5faeda1a28ea0e4
Justin Obara <obara.justin@gmail.com>
2012-06-22 13:01:05 -0700

    DECA-277: Reorganized and added comments to decapod.py. The classes are now in order of use/url segment. This is in preparation of updating export resource to handle image export.

--- decapod.py
+++ decapod.py
@@ -26,7 +26,74 @@ bgtask.subscribe()
 def setJSONResponseHeaders(fileName="model.json"):
     cherrypy.response.headers["Content-Type"] = "application/json"
     cherrypy.response.headers["Content-Disposition"] = "attachment; filename='{0}'".format(fileName)
+       
+def startServer():
+    '''
+    Starts the cherrypy server and sets up the necessary signal handlers for running from the command line
+    '''
+    # mount app
+    root = mountApp()
 
+    # subscribe to a signal handler.
+    # used for quitting the app via command line
+    if hasattr(cherrypy.engine, 'signal_handler'):
+        cherrypy.engine.signal_handler.subscribe()
+        
+    # subscribe to a console control handler.
+    # used for quitting the app via command line on windows   
+    if hasattr(cherrypy.engine, "console_control_handler"): 
+        cherrypy.engine.console_control_handler.subscribe() 
+    
+    # start the server
+    cherrypy.engine.start()
+    
+    # Block if running standalone, nut under an SCGI-SWGI interface
+    if (not IS_SCGIWSGI) :
+        cherrypy.engine.block()
+        
+def mountApp(config=DECAPOD_CONFIG_FILE):
+    '''
+    Mounts the app and updates the cherryp configuration from the provided config file.
+    Is used by startServer to start the cherrypy server, but can be called independently 
+    if another process starts cherrypy (e.g. when run in the unit tests).
+    '''
+    # Set up the server application and its controllers
+    root = DecapodServer()
+    root.library = LibraryController()
+    
+    # update the servers configuration (e.g. sever.socket_host)
+    cherrypy.config.update(config)
+    
+    # mount the app
+    cherrypy.tree.mount(root, "/", config)
+    return root
+
+class DecapodServer(object):
+    '''
+    Handler for the / resource
+    
+    Redirects requests to the root url to the apps start page.
+    '''
+    exposed = True
+    book = []
+        
+    def GET(self):
+        raise cherrypy.HTTPRedirect(rs.url("${components}/exporter/html/exporter.html"), 301)
+    
+# Library Controller
+# TODO: Support GET requests to return the Library model
+# TODO: Support POST requests to create a Book and return the new Library model
+class LibraryController(object):
+    '''
+    Parses the positional arguments starting after /library/
+    and calls the appropriate handlers for the various resources 
+    '''
+    exposed = True
+    
+    # Continues cherrypy object traversal. Useful for handling dynamic URLs
+    def _cp_dispatch(self, vpath):
+        return BookController(vpath[0])
+    
 #TODO: Support GET requests to return the book's model
 #TODO: Support PUT requests to update the library's model
 #TODO: Update DELETE to remove the entire book directory, by name, and not just all the stored pages
@@ -113,60 +180,6 @@ class ExportController(object):
         self.export.deletePDF()
         cherrypy.response.status = 204
 
-# Library Controller
-# TODO: Support GET requests to return the Library model
-# TODO: Support POST requests to create a Book and return the new Library model
-class LibraryController(object):
-    '''
-    Parses the positional arguments starting after /library/
-    and calls the appropriate handlers for the various resources 
-    '''
-    exposed = True
-    
-    # Continues cherrypy object traversal. Useful for handling dynamic URLs
-    def _cp_dispatch(self, vpath):
-        return BookController(vpath[0])
-
-class DecapodServer(object):
-    exposed = True
-    book = []
-        
-    def GET(self):
-        raise cherrypy.HTTPRedirect(rs.url("${components}/exporter/html/exporter.html"), 301)
-    
-def mountApp(config=DECAPOD_CONFIG_FILE):
-    # Set up the server application and its controllers
-    root = DecapodServer()
-    root.library = LibraryController()
-    
-    # update the servers configuration (e.g. sever.socket_host)
-    cherrypy.config.update(config)
-    
-    # mount the app
-    cherrypy.tree.mount(root, "/", config)
-    return root
-        
-def startServer():
-    # mount app
-    root = mountApp()
-
-    # subscribe to a signal handler.
-    # used for quitting the app via command line
-    if hasattr(cherrypy.engine, 'signal_handler'):
-        cherrypy.engine.signal_handler.subscribe()
-        
-    # subscribe to a console control handler.
-    # used for quitting the app via command line on windows   
-    if hasattr(cherrypy.engine, "console_control_handler"): 
-        cherrypy.engine.console_control_handler.subscribe() 
-    
-    # start the server
-    cherrypy.engine.start()
-    
-    # Block if running standalone, nut under an SCGI-SWGI interface
-    if (not IS_SCGIWSGI) :
-        cherrypy.engine.block()
-    
 if __name__ == "__main__":
     startServer()
     
