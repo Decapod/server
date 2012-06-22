1775241e38b357afa06db0ac796962c98cccfca5
Justin Obara <obara.justin@gmail.com>
2012-06-22 10:52:58 -0700

    DECA-277: Updated the image.py script and unit tests. Now should support converting to any format that imagemagicks convert function does.

--- image.py
+++ image.py
@@ -11,50 +11,52 @@ TEMP_DIR = TIFF_DIR + "temp/"
 statusFileName = "exportStatus.json"
 tiffDir = "tiffTemp"
 
-class TIFFConversionError(Exception): pass
-class TIFFImageError(Exception): pass
-class TIFFOutputPathError(Exception): pass
+class ConversionError(Exception): pass
+class ImageError(Exception): pass
+class OutputPathError(Exception): pass
 
-def convertImage(imagePath, outputDir=None):
+def convert(imagePath, format, outputDir=None):
     '''
-    Converts the image at imagePath into a tiff image.
+    Converts the image at imagePath into the specified image format
     Can optionaly specify the directory where to save the new file, will default to the same directory as the original file.
+    If the format is invalid, it will convert it to jpeg
     Returns the path to the new image file.
     
     Exceptions:
-    TIFFImageError: if the file at imagePath isn't a supported image format
-    TIFFConversionError: an error occurs during the conversion process
+    ImageError: if the file at imagePath isn't a supported image format
+    ConversionError: an error occurs during the conversion process
     '''
     if utils.isImage(imagePath):
-        ext = ".tiff"
+        ext = format if format[0] == "." else "." + format
         origDir, fileName = os.path.split(imagePath)
         name = os.path.splitext(fileName)[0]
         writeDir = outputDir if outputDir != None else origDir
         
         writePath = os.path.join(writeDir, name + ext)
-        utils.invokeCommandSync(["convert", imagePath, writePath], TIFFConversionError, "Error converting {0} to TIFF".format(imagePath))
+        utils.invokeCommandSync(["convert", imagePath, writePath], ConversionError, "Error converting {0} to '{1}' format".format(imagePath, format))
         return writePath
     else:
-        raise TIFFImageError("{0} is not a valid image file".format(imagePath))
+        raise ImageError("{0} is not a valid image file".format(imagePath))
 
-def convertImages(imagePaths, outputDir=None):
+def batchConvert(imagePaths, format, outputDir=None):
     '''
-    Converts the images in the list of imagePaths to tiff format.
+    Converts the image at imagePath into the specified image format
     Can optionaly specify the directory where the converted images will be saved, will default to putting them back into their original directories.
     If the path provided is not an image, it will be ignored.
+    If the format is invalid, it will convert it to jpeg
     Returns a list of the paths to the new image files
     
     Exceptions:
-    TIFFConversionError: an error occurs during the conversion process
+    ConversionError: an error occurs during the conversion process
     '''
     convertedImages = []
     for imagePath in imagePaths:
         if utils.isImage(imagePath):
-            convertedImage = convertImage(imagePath, outputDir)
+            convertedImage = convert(imagePath, format, outputDir)
             convertedImages.append(convertedImage)
     return convertedImages
 
-def convertAndZipImages(imagePaths, outputPath, tempDir=None):
+def archiveConvert(imagePaths, format, archivePath, tempDir=None):
     '''
     Converts the images in the list of imagePaths to tiff format.
     Must specify the output path, including the file name, for the zip file to be saved.
@@ -66,28 +68,28 @@ def convertAndZipImages(imagePaths, outputPath, tempDir=None):
     If no valid images were provided, None is returned
     
     Exceptions:
-    TIFFConversionError: an error occurs during the conversion process
-    TIFFOutputPathError: the output path is malformed (e.g. path to a directory)
+    ConversionError: an error occurs during the conversion process
+    OutputPathError: the output path is malformed (e.g. path to a directory)
     '''
-    zipPath = outputPath
+    result = archivePath
     currentDir = os.getcwd()
     temp = tempDir if tempDir != None else os.path.join(os.getcwd(), "temp")
     utils.makeDirs(tempDir)
-    converted = convertImages(imagePaths, temp)
+    converted = batchConvert(imagePaths, format, temp)
     os.chdir(tempDir) # Need to change to the tempDir where the images are so that the zip file won't contain any directory structure
     if len(converted) > 0:
         try:
-            zip = zipfile.ZipFile(outputPath, mode="a")
+            zip = zipfile.ZipFile(archivePath, mode="a")
         except IOError:
-            raise TIFFOutputPathError("{0} is not a valid path".format(outputPath))
+            raise OutputPathError("{0} is not a valid path".format(archivePath))
         for imagePath in converted:
             imageFile = os.path.split(imagePath)[1] # extacts the filename from the path
             zip.write(imageFile)
         zip.close()
     else:
         # Return None when there are no valid image paths
-        zipPath = None
+        result = None
     
     os.chdir(currentDir)
     utils.rmTree(tempDir)
-    return zipPath
+    return result
