3bff8fec54bfefb8eb1dc62035edf2c3203a24f1
Justin Obara <obara.justin@gmail.com>
2012-09-14 15:00:13 -0400

    DECA-308: Added the mockCameraInterface to be used for testing. This will remove the need for real cameras to be attached to the machine.

---
+++ capture/mockCameraInterface.py
@@ -0,0 +1,66 @@
+from __future__ import division
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join('..', 'utils')))
+import resourcesource as rs
+import utils
+
+class InvalidPortError(Exception): pass
+
+CAMERA_INFO_BY_PORT = {
+    "usb:001,002": {
+        "model": "Canon PowerShot G10",
+        "captureFormats": "JPEG",
+        "resolution": 14.6,
+        "capabilities": "File Download, File Deletion, File Upload\n\tGeneric Image Capture, No Open Capture, Canon Capture"
+    },
+    "usb:001,003": {
+        "model": "Canon PowerShot G10",
+        "captureFormats": "JPEG",
+        "resolution": 14.6,
+        "capabilities": "File Download, File Deletion, File Upload\n\tGeneric Image Capture, No Open Capture, Canon Capture"
+    }
+};
+
+def detectCameras():
+    cameras = []
+    
+    for port in CAMERA_INFO_BY_PORT:
+        cameras.append({"model": CAMERA_INFO_BY_PORT[port]["model"], "port": port})
+
+    return cameras
+
+def isPortValid(port):
+    return port in CAMERA_INFO_BY_PORT
+    
+def getCameraSummaryByPort(port):
+    summary = {}
+    
+    if (isPortValid(port)):
+        summary = CAMERA_INFO_BY_PORT[port]
+
+    return summary
+
+def getAllCamerasSummary():
+    allInfo = {}
+    allInfo["cameras"] = []
+    
+    connectedCameras = detectCameras()
+    
+    for camera in connectedCameras:
+        allInfo['cameras'].append(getCameraSummaryByPort(camera["port"]))
+    
+    return allInfo
+
+def capture(port, filename, dir="images"):
+    '''
+    Not yet implemented
+    '''
+    if (isPortValid(port)):
+        return None
+    else: 
+        raise InvalidPortError
+
+def getResolution(port):
+    return CAMERA_INFO_BY_PORT[port]["resolution"]
