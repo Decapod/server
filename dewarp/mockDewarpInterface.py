5e13ad732229285904c3557832af5e43069a4dcc
Cindy Qi Li <cli@ocadu.ca>
2012-10-09 15:37:11 -0400

    DECA-316: More code clean up.

--- dewarp/mockDewarpInterface.py
+++ dewarp/mockDewarpInterface.py
@@ -4,8 +4,8 @@ import shutil
 
 MOCK_DATA_DIR = os.path.abspath("mockData/")
 
-def dewarpPair(calibrationDir, dewarpoedPath, img1, img2):
+def dewarpPair(calibrationDir, dewarpedPath, img1, img2):
     imgDir = os.path.join(MOCK_DATA_DIR)
     
-    shutil.copyfile(os.path.join(imgDir, "capture-0_1.jpg"), os.path.join(dewarpoedPath, "capture-0_1.jpg"))
-    shutil.copyfile(os.path.join(imgDir, "capture-0_2.jpg"), os.path.join(dewarpoedPath, "capture-0_2.jpg"))
+    shutil.copy(os.path.join(imgDir, "capture-0_1.jpg"), dewarpedPath)
+    shutil.copy(os.path.join(imgDir, "capture-0_2.jpg"), dewarpedPath)
