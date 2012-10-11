e53d21b65e68bdb6d147b5e840da131f425a50a0
Cindy Qi Li <cli@ocadu.ca>
2012-10-11 14:46:24 -0400

    NOJIRA: Merged in Justin's changes.

---
+++ export/tests/test_book.py
@@ -0,0 +1,43 @@
+import sys
+import os
+import unittest
+
+sys.path.append(os.path.abspath(os.path.join('..')))
+sys.path.append(os.path.abspath(os.path.join('..', '..', 'utils')))
+import book
+from mockClasses import mockResourceSource
+from utils import io
+
+LIBRARY_PATH = os.path.abspath("data/library/")
+BOOK_DIR = os.path.join(LIBRARY_PATH, "book")
+
+class TestBookExisting(unittest.TestCase):
+    
+    book = None
+    mockRS = mockResourceSource({"/library": {"path": LIBRARY_PATH}})
+    
+    def setUp(self):
+        self.book = book.Book(self.mockRS)
+        io.makeDirs(BOOK_DIR)
+    
+    def test_01_delete(self):
+        self.assertTrue(os.path.exists(BOOK_DIR), "The 'book' directory (at path: {0}) should currently exist".format(BOOK_DIR))
+        self.book.delete()
+        self.assertFalse(os.path.exists(BOOK_DIR), "The 'book' directory (at path: {0}) should have been removed".format(BOOK_DIR))
+
+class TestBookNone(unittest.TestCase):
+    
+    book = None
+    mockRS = mockResourceSource({"/library": {"path": BOOK_DIR}})
+    
+    def setUp(self):
+        self.book = book.Book(self.mockRS)
+        io.rmTree(BOOK_DIR)
+            
+    def test_01_delete(self):
+        self.assertFalse(os.path.exists(BOOK_DIR), "The 'book' directory (at path: {0}) should have been removed".format(BOOK_DIR))
+        self.book.delete()
+        self.assertFalse(os.path.exists(BOOK_DIR), "The 'book' directory (at path: {0}) should have been removed".format(BOOK_DIR))
+
+if __name__ == '__main__':
+    unittest.main()
