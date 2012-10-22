dfcbf161ae4dc2c887fe72f2e1d25ae979503d50
Justin Obara <obara.justin@gmail.com>
2012-10-22 09:41:02 -0400

    DECA-316: Updated the dewarp server to redirect to dewarper.html that was recently added.

---
+++ dewarp/dewarpProcessor.py
@@ -0,0 +1,219 @@
+import os
+import sys
+import re
+from string import Template
+
+sys.path.append(os.path.abspath(os.path.join('..', 'utils')))
+import dewarpInterface
+import mockDewarpInterface
+import utils
+from status import Status
+from store import FSStore
+
+#constants for statuses
+EXPORT_IN_PROGRESS = "in progress"
+EXPORT_COMPLETE = "complete"
+EXPORT_READY = "ready"
+EXPORT_ERROR = "error"
+
+DEFAULT_CAPTURE_NAME_TEMPLATE = "capture-${captureIndex}_${cameraID}"
+
+# Exception classes
+class DewarpError(Exception): pass
+class DewarpInProgressError(Exception): pass
+class UnpackedDirNotExistError(Exception): pass
+
+class DewarpProcessor(object):
+    
+    def __init__(self, dataDir, statusFile, testmode=False):
+        self.dataDir = dataDir
+        self.unpackedDir = os.path.join(self.dataDir, "unpacked")
+        self.dewarpedDir = os.path.join(self.dataDir, "dewarped")
+        self.calibrationDir = os.path.join(self.unpackedDir, "calibration")
+        self.export = os.path.join(self.dataDir, "export.zip")
+        self.statusFilePath = statusFile
+        self.dewarpController = mockDewarpInterface if testmode else dewarpInterface
+
+        utils.io.makeDirs(self.dataDir)
+        self.status = Status(FSStore(self.statusFilePath), {"status": EXPORT_READY})  
+
+    def isInState(self, state):
+        return self.status.model["status"] == state
+
+    def getStatus(self):        
+        return self.status.model
+    
+    def getCapturesStatus(self, filenameTemplate=DEFAULT_CAPTURE_NAME_TEMPLATE):
+        if not os.path.exists(self.unpackedDir):
+            raise UnpackedDirNotExistError, "The directory \"{0}\" for the unpacked dewarping zip does not exist.".format(self.unpackedDir)
+        
+        if not os.path.exists(self.calibrationDir):
+            return self.constructErrorStatus("CalibrationDirNotExist", "The calibration directory does not exist.")
+
+        matched, unmatched = self.findPairs(self.unpackedDir, filenameTemplate)
+        
+        if unmatched:
+            return self.constructErrorStatus("UnmatchedPairs", map(os.path.basename, unmatched))
+        
+        return self.constructSucessStatus(len(matched))
+    
+    def delete(self):
+        '''
+        Removes the export artifacts.
+        
+        Exceptions
+        ==========
+        DewarpInProgressError: if dewarping is currently in progress
+        '''
+        if self.isInState(EXPORT_IN_PROGRESS):
+            raise DewarpInProgressError, "Dewarping in progress, cannot delete until this process has finished"
+        else:
+            utils.io.rmTree(self.dewarpedDir)
+            self.status.update("status", EXPORT_READY)
+            self.status.remove("percentage")
+            self.status.remove("currentCapture")
+
+    def deleteUpload(self):
+        '''
+        Removes the export artifacts.
+        
+        Exceptions
+        ==========
+        DewarpInProgressError: if dewarping is currently in progress
+        '''
+        if self.isInState(EXPORT_IN_PROGRESS):
+            raise DewarpInProgressError, "Dewarping in progress, cannot delete until this process has finished"
+        else:
+            utils.io.rmTree(self.unpackedDir)
+
+    def unzip(self, file, filenameTemplate=DEFAULT_CAPTURE_NAME_TEMPLATE):
+        '''
+        Unzips the uploaded archive and returns a dict of status.
+        
+        Exceptions
+        ==========
+        DewarpInProgressError: if dewarping is currently in progress
+        '''
+        
+        if self.isInState(EXPORT_IN_PROGRESS):
+            raise DewarpInProgressError, "Dewarping currently in progress, cannot accept another zip until this process has finished"
+        
+        fileType = utils.io.getFileType(file)
+        
+        if fileType != "zip":
+            return self.constructErrorStatus("BadZip", "The zip file is invalid.")
+        
+        name = file.filename if file.filename else utils.io.generateFileName(suffix=fileType)
+        zipfilePath = os.path.join(self.dataDir, file.filename)
+        utils.io.writeStreamToFile(file, zipfilePath)
+            
+        try:
+            utils.io.unzip(zipfilePath, self.unpackedDir)
+        except Exception:
+            utils.io.rmFile(zipfilePath)
+            return self.constructErrorStatus("BadZip", "The zip file is invalid.")
+        
+        utils.io.rmFile(zipfilePath)
+        return self.getCapturesStatus(filenameTemplate)
+        
+    def dewarp(self):
+        '''
+        Generates the pdf export.
+        If an exception is raised from the genPDF subprocess the status will be set to EXPORT_ERROR
+        
+        Exceptions
+        ==========
+        DewarpInProgressError: if dewarping is currently in progress
+        DewarpError: if unable to complete dewarping 
+        '''
+        
+        if self.isInState(EXPORT_IN_PROGRESS):
+            raise DewarpInProgressError, "Dewarping currently in progress, cannot accept another zip until this process has finished"
+        else:
+            # perform dewarp, update status including percentage complete
+            self.status.update("status", EXPORT_IN_PROGRESS)
+            try:
+                self.dewarpImp(self.unpackedDir, self.dewarpedDir)
+            except Exception as e:
+                self.status.update("status", EXPORT_ERROR)
+                self.status.remove("percentage")
+                self.status.remove("currentCapture")
+                utils.io.rmTree(self.dewarpedDir)
+                
+                raise DewarpError, e.message
+            
+            self.status.update("status", EXPORT_COMPLETE)
+    
+    def dewarpImp(self, unpackedDir, dewarpedDir, filenameTemplate=DEFAULT_CAPTURE_NAME_TEMPLATE):
+        '''
+        Dewarps the paired images in the from directory "unpackedDir" and output to "dewarpedDir"
+        
+        The "unpackedDir" contains all the paired images and the calibration information
+        The "dewarpedDir" is the output directory of all the dewarped images. It will be created if it does not exist.
+        
+        Exceptions:
+        ==========
+        UnpackedDirNotExistError: If the directory for the unpacked dewarping zip does not exist
+        '''
+        
+        if not os.path.exists(unpackedDir):
+            raise UnpackedDirNotExistError, "The directory \"{0}\" for the unpacked dewarping zip does not exist.".format(unpackedDir)
+        
+        matched, unmatched = self.findPairs(unpackedDir, filenameTemplate)
+        
+        if matched:
+            if not os.path.exists(dewarpedDir):
+                os.mkdir(dewarpedDir)
+                
+            self.status.update("percentage", 0)
+            self.status.update("currentCapture", 0)
+            numOfMatches = len(matched)
+            
+            for index, (img1, img2) in enumerate(matched):
+                captureIndex = index + 1
+                self.dewarpController.dewarpPair(self.calibrationDir, dewarpedDir, img1, img2)
+                self.status.update("percentage", int(captureIndex/numOfMatches*100))
+                self.status.update("currentCapture", captureIndex)
+    
+        return True
+    
+    def findPairs(self, unpackedDir, filenameTemplate=DEFAULT_CAPTURE_NAME_TEMPLATE):
+        unmatched = []
+        matched = []
+        
+        regexPattern = Template(filenameTemplate).safe_substitute(cameraID="\d*", captureIndex="\d*")
+        allImages = utils.image.findImages(unpackedDir, regexPattern, False)
+        
+        sortedAllImages = sorted(allImages)
+        
+        # find the maximum captureIndex
+        while len(sortedAllImages) > 0:
+            currentImg = sortedAllImages.pop(0)
+            
+            imgPrefix, theRest = currentImg.split("_")
+            
+            if imgPrefix and theRest:
+                imgMatchPattern = "^" + imgPrefix
+                regex = re.compile(imgMatchPattern)
+                
+                numOfImages = len(sortedAllImages)
+                
+                if numOfImages > 0 and regex.findall(sortedAllImages[0]):
+                    pairImg = sortedAllImages[0]
+                
+                    # Toss out the pair image to prevent the further comparison
+                    sortedAllImages.pop(0)
+                    matched.append((currentImg, pairImg))
+                else:
+                    unmatched.append(currentImg)
+            else:
+                unmatched.append(currentImg)
+                
+        return matched, unmatched
+    
+    def constructErrorStatus(self, errorCode, msg):
+        return {"ERROR_CODE": errorCode, "msg": msg}
+
+    def constructSucessStatus(self, numOfCaptures):
+        return {"numOfCaptures": numOfCaptures}
+    
---
+++ export/exportServer.py
@@ -0,0 +1,257 @@
+
+# TODO: File system or CouchDB persistence for the Book, including:
+#   * the images array
+
+import cherrypy
+import os
+import sys
+import simplejson as json
+
+import imageImport
+import book
+import pdf
+import image
+sys.path.append(os.path.abspath(os.path.join('..', 'utils')))
+import backgroundTaskQueue
+import resourcesource as rs
+from utils import server
+
+# Setup for Decapod's cherrypy configuration file.
+CURRENT_DIR = os.getcwd()
+DECAPOD_CONFIG_FILE = os.path.abspath("config/exportServer.conf")
+
+# Determine if the server is running under an SCGI-WSGI interface
+IS_SCGIWSGI = (sys.argv[0] == 'scgi-wsgi')
+
+#subscribe to BackgroundTaskQueue plugin
+bgtask = backgroundTaskQueue.BackgroundTaskQueue(cherrypy.engine)
+bgtask.subscribe()
+       
+def startServer():
+    '''
+    Starts the cherrypy server and sets up the necessary signal handlers for running from the command line
+    '''
+    # mount app
+    root = mountApp()
+
+    # subscribe to a signal handler.
+    # used for quitting the app via command line
+    if hasattr(cherrypy.engine, 'signal_handler'):
+        cherrypy.engine.signal_handler.subscribe()
+        
+    # subscribe to a console control handler.
+    # used for quitting the app via command line on windows   
+    if hasattr(cherrypy.engine, "console_control_handler"): 
+        cherrypy.engine.console_control_handler.subscribe() 
+    
+    # start the server
+    cherrypy.engine.start()
+    
+    # Block if running standalone, nut under an SCGI-SWGI interface
+    if (not IS_SCGIWSGI) :
+        cherrypy.engine.block()
+        
+def mountApp(config=DECAPOD_CONFIG_FILE):
+    '''
+    Mounts the app and updates the cherryp configuration from the provided config file.
+    Is used by startServer to start the cherrypy server, but can be called independently 
+    if another process starts cherrypy (e.g. when run in the unit tests).
+    '''
+    # Set up the server application and its controllers
+    root = ExportServer()
+    root.library = LibraryController()
+    
+    # update the servers configuration (e.g. sever.socket_host)
+    cherrypy.config.update(config)
+    
+    # mount the app
+    cherrypy.tree.mount(root, "/", config)
+    return root
+
+class ExportServer(object):
+    '''
+    Handler for the / resource
+    
+    Redirects requests to the root url to the apps start page.
+    '''
+    exposed = True
+    book = []
+        
+    def GET(self):
+        raise cherrypy.HTTPRedirect(rs.url("${export}/components/exporter/html/exporter.html"), 301)
+    
+# Library Controller
+# TODO: Support GET requests to return the Library model
+# TODO: Support POST requests to create a Book and return the new Library model
+class LibraryController(object):
+    '''
+    Parses the positional arguments starting after /library/
+    and calls the appropriate handlers for the various resources 
+    '''
+    exposed = True
+    
+    # Continues cherrypy object traversal. Useful for handling dynamic URLs
+    def _cp_dispatch(self, vpath):
+        return BookController(vpath[0])
+    
+#TODO: Support GET requests to return the book's model
+#TODO: Support PUT requests to update the library's model
+#TODO: Update DELETE to remove the entire book directory, by name, and not just all the stored pages
+class BookController(object):
+    '''
+    Handler for the /library/"bookName"/ resource
+    
+    Currently only handles DELETE requests, which result in all the pages being removed.
+    '''
+    exposed = True
+
+    def __init__(self, name):
+        self.name = name
+        self.book = book.Book()
+        self.paths = {
+            "pages": PagesController(self.name),
+            "export": ExportController(self.name)
+        }
+        
+    def DELETE(self, *args, **kwargs):
+        self.book.delete()
+        cherrypy.response.status = 204
+    
+    # Continues cherrypy object traversal. Useful for handling dynamic URLs
+    def _cp_dispatch(self, vpath):
+        pathSegment = vpath[0]
+        
+        if pathSegment in self.paths:
+            return self.paths[pathSegment]
+
+#TODO: Support GET requests to return the book's pages model
+#TODO: Support PUT requests to update the book's pages model
+#TODO: Update POST to save pages to the specified book, instead of to a global location
+#TODO: On POST do more than just save the images, model data should also be added
+class PagesController(object):
+    '''
+    Handler for the /library/"bookName"/pages resource
+    
+    Currently only handles POST requests, which results in new pages being added.
+    This is useful for importing pages.
+    '''
+    exposed = True
+    page = None
+    
+    def __init__(self, bookName):
+        self.bookName = bookName
+        self.page = imageImport.ImageImport()
+    
+    # TODO: Should return the weburl not the filesystem path
+    def POST(self, *args, **kwargs):
+        # import the file
+        try:
+            path = self.page.save(kwargs["file"])
+        except imageImport.ImportTypeError as e:
+            raise cherrypy.HTTPError(415, e.message)
+        cherrypy.response.headers["Location"] = path
+        cherrypy.response.status = 201
+
+class ExportController(object):
+    '''
+    Handler for the /library/"bookName"/export resource
+    '''
+    exposed = True
+
+    def __init__(self, bookName):
+        self.bookName = bookName
+        self.paths = {
+            "pdf": PDFExportController(self.bookName),
+            "image": ImageExportController(self.bookName)
+        }
+    
+    # Continues cherrypy object traversal. Useful for handling dynamic URLs
+    def _cp_dispatch(self, vpath):
+        pathSegment = vpath[0]
+        
+        if pathSegment in self.paths:
+            return self.paths[pathSegment]
+        
+class PDFExportController(object):
+    '''
+    Handler for the /library/"bookName"/export/pdf resource
+    '''
+    exposed = True
+    
+    def __init__(self, bookName):
+        self.bookName = bookName
+        self.export = pdf.PDFGenerator()
+        self.typeKey = "type"
+        
+    def GET(self, *args, **kwargs):
+        #returns the status and, if available, the url to the exported pdf
+        server.setAttachmentResponseHeaders(cherrypy, "exportStatus.json", "application/json")
+        return json.dumps(self.export.getStatus())
+        
+    def PUT(self, *args, **kwargs):
+        #triggers the creation of the pdf export
+
+        # ensures that the type argument exists
+        if len(args) is 0:
+            raise cherrypy.HTTPError(405)
+        
+        exportType = args[0]
+        options = kwargs
+        
+        # ensures that the type argument starts with the "type" keyword
+        if not exportType[0:4] == self.typeKey:
+            raise cherrypy.HTTPError(400)
+        
+        options[self.typeKey] = exportType[4:] 
+        bgtask.put(self.export.generate, options)
+        cherrypy.response.status = 202
+        server.setAttachmentResponseHeaders(cherrypy, "exportStatus.json", "application/json")
+        return json.dumps(self.export.getStatus())
+    
+    def DELETE(self, *args, **kwargs):
+        #removes the pdf export artifact
+        try:
+            self.export.deletePDF()
+        except pdf.PDFGenerationInProgressError as e:
+            raise cherrypy.HTTPError(409, e.message)
+        cherrypy.response.status = 204
+        
+class ImageExportController(object):
+    '''
+    Handler for the /library/"bookName"/export/image resource
+    '''
+    exposed = True
+    
+    def __init__(self, bookName):
+        self.bookName = bookName
+        self.nameTemplate = "Image-$index" #TODO: Move to configuration or take in as an argument
+        self.exporter = image.ImageExporter(nameTemplate=self.nameTemplate)
+        
+    def GET(self, *args, **kwargs):
+        #returns the status and, if available, the url to the exported pdf
+        server.setAttachmentResponseHeaders(cherrypy, "exportStatus.json", "application/json")
+        return json.dumps(self.exporter.getStatus())
+        
+    def PUT(self, *args, **kwargs):
+        #triggers the creation of the pdf export
+        
+        # ensures that the format argument exists
+        if len(args) is 0:
+            raise cherrypy.HTTPError(405)
+        
+        bgtask.put(self.exporter.export, args[0])
+        cherrypy.response.status = 202
+        server.setAttachmentResponseHeaders(cherrypy, "exportStatus.json", "application/json")
+        return json.dumps(self.exporter.getStatus())
+    
+    def DELETE(self, *args, **kwargs):
+        #removes the pdf export artifact
+        try:
+            self.exporter.deleteExport()
+        except image.ExportInProgressError as e:
+            raise cherrypy.HTTPError(409, e.message)
+        cherrypy.response.status = 204
+
+if __name__ == "__main__":
+    startServer()
+    
