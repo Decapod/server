6764007cf2925dda794d35b87873313ff4c6c370
Cindy Qi Li <cli@ocadu.ca>
2012-11-21 12:44:40 -0500

    DECA-316: Fixed the wrong dewarped dir at invoking dewarp command.

--- dewarp/dewarpInterface.py
+++ dewarp/dewarpInterface.py
@@ -4,8 +4,6 @@ import sys
 sys.path.append(os.path.abspath(os.path.join('..', 'utils')))
 import utils
 
-DEWARP_PREFIX = "dewarped"
-
 # Exception classes
 class DewarpError(Exception): pass
 
@@ -13,5 +11,5 @@ def dewarpPair(calibrationDir, dewarpedPath, img1, img2):
 
     # Invokes the dewarping command
     executable = os.path.join("..", "..", "decapod-dewarping", "dewarp.py")
-    cmd = [executable, calibrationDir, img1, img2, DEWARP_PREFIX]
+    cmd = [executable, calibrationDir, img1, img2, dewarpedPath]
     utils.io.invokeCommandSync(cmd, DewarpError, "Error at dewarping")
