0181d475c1ea60814016bcdcf7a4b76053f664e1
Cindy Qi Li <cli@ocadu.ca>
2012-11-21 11:09:10 -0500

    DECA-316: Adjusted the path to dewarp.py.

--- dewarp/dewarpInterface.py
+++ dewarp/dewarpInterface.py
@@ -12,7 +12,6 @@ class DewarpError(Exception): pass
 def dewarpPair(calibrationDir, dewarpedPath, img1, img2):
 
     # Invokes the dewarping command
-    # TODO: Need to update the path when the location of dewarp.py is determined
-    executable = os.path.join("..", "..", "..", "..", "decapod-dewarping", "dewarp.py")
+    executable = os.path.join("..", "..", "decapod-dewarping", "dewarp.py")
     cmd = [executable, calibrationDir, img1, img2, DEWARP_PREFIX]
     utils.io.invokeCommandSync(cmd, DewarpError, "Error at dewarping")
