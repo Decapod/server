49fdf7dea41110fa9a8c1e92c64d0a2e44e9565a
Justin Obara <obara.justin@gmail.com>
2012-11-27 13:53:43 -0500

    DECA-316: changed the dewarping to expect the .colors file to be created in the working directory.

--- dewarp/mockColourPickerInterface.py
+++ dewarp/mockColourPickerInterface.py
@@ -7,5 +7,5 @@ import utils
 def launchColourPicker(img):
 
     # Invokes the colourPicker command
-    writePath = os.path.join("..", "..", "..", "decapod-dewarping", ".colors")
+    writePath = os.path.join(os.getcwd(), ".colors")
     utils.io.writeToFile("55 24 21\n5 5 4", writePath)
